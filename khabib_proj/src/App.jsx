87f46fe2beaeec4dececb6077bccf112ae958a1d
Khamz1 <khamzzaty@mail.ru>
2025-05-05 10:53:22 +0300

    точно точно final

--- khabib_proj/src/App.jsx
+++ khabib_proj/src/App.jsx
@@ -4,7 +4,7 @@ import {useAuthStore} from "./store/auth.store.js";
 import Header from "./components/Layout/Header/Header.jsx";
 import PostList from "./components/Posts/PostList.jsx";
 import Home from './pages/Home/Home.jsx'
-import {Routing} from "@pages/routing/Routing.jsx";
+import {Routing} from "../src/pages/routing/Routing.jsx";
 
 function App() {
     const { isAuthenticated, fetchProfile } = useAuthStore();
